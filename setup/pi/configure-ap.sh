24871796ceb18932ba2e6345e36023793910abc1
jjermany <125428573+jjermany@users.noreply.github.com>
2025-03-03 20:13:52 -0600

    Update configure-ap.sh

--- setup/pi/configure-ap.sh
+++ setup/pi/configure-ap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -eu
 
+# based on https://blog.thewalr.us/2017/09/26/raspberry-pi-zero-w-simultaneous-ap-and-managed-mode-wifi/
+
 function log_progress () {
   if declare -F setup_progress > /dev/null
   then
@@ -24,10 +26,7 @@ fi
 function nm_get_wifi_client_device () {
   for i in {1..5}
   do
-    WLAN="$(nmcli -t -f TYPE,DEVICE c show --active | grep 802-11-wireless | grep -v ":ap0$" | grep "wlan1" | cut -c 17-)"
-    if [ -z "$WLAN" ]; then
-      WLAN="$(nmcli -t -f TYPE,DEVICE c show --active | grep 802-11-wireless | grep -v ":ap0$" | grep "wlan0" | cut -c 17-)"
-    fi
+    WLAN="$(nmcli -t -f TYPE,DEVICE c show --active | grep 802-11-wireless | grep -v ":ap0$" | cut -c 17-)"
     if [ -n "$WLAN" ]
     then
       break;
@@ -46,16 +45,24 @@ function nm_get_wifi_client_device () {
 function nm_add_ap () {
   nm_get_wifi_client_device || return 1
 
+  if [ "$WLAN" = "wlan1" ]; then
+    log_progress "Switching wlan1 from client mode to AP mode"
+    nmcli dev set wlan1 managed no
+    ip link set wlan1 down
+    iw dev wlan1 set type __ap
+    ip link set wlan1 up
+  fi
+
   if ! iw dev ap0 info &> /dev/null
   then
-    # Create additional virtual interface for the wifi device
     iw dev "$WLAN" interface add ap0 type __ap || return 1
   fi
 
-  # Disable power saving for AP
+  # Disable power saving mode
   iw "$WLAN" set power_save off || return 1
   iw ap0 set power_save off || return 1
 
+  # Set up the AP
   nmcli con delete TESLAUSB_AP &> /dev/null || true
   nmcli con add type wifi ifname ap0 mode ap con-name TESLAUSB_AP ssid "$AP_SSID" || return 1
   nmcli con modify TESLAUSB_AP 802-11-wireless-security.key-mgmt wpa-psk || return 1
@@ -65,24 +72,13 @@ function nm_add_ap () {
   nmcli con modify TESLAUSB_AP ipv4.method shared || return 1
   nmcli con modify TESLAUSB_AP ipv6.method disabled || return 1
 
-  cat > /etc/network/if-up.d/teslausb-ap << EOF
-#!/bin/bash
-
-if [ "\$IFACE" = "$WLAN" ]
-then
-  iw dev $WLAN interface add ap0 type __ap
-  iw "$WLAN" set power_save off
-  iw ap0 set power_save off
-  nmcli con up TESLAUSB_AP
-fi
-
-EOF
-  chmod a+x /etc/network/if-up.d/teslausb-ap || return 1
+  log_progress "AP setup completed on $WLAN."
 }
+
 
 if systemctl --quiet is-enabled NetworkManager.service
 then
-  apt-get -y --force-yes install iw || return 1
+  apt-get -y install iw || return 1
   if ! nm_add_ap
   then
     log_progress "Retrying after restarting Network Manager"
@@ -97,6 +93,7 @@ then
   exit 0
 fi
 
+
 if [ ! -e /etc/wpa_supplicant/wpa_supplicant.conf ]
 then
   log_progress "No wpa_supplicant, skipping AP setup."
@@ -108,11 +105,20 @@ then
   IP=${AP_IP:-"192.168.66.1"}
   NET=$(echo -n "$IP" | sed -e 's/\.[0-9]\{1,3\}$//')
 
-  log_progress "installing dnsmasq and hostapd"
-  apt-get -y --force-yes install dnsmasq hostapd
+  log_progress "Installing dnsmasq and hostapd"
+  apt-get -y install dnsmasq hostapd
 
-  log_progress "configuring AP '$AP_SSID' with IP $IP"
-  MAC="$(cat /sys/class/net/wlan1/address 2>/dev/null || cat /sys/class/net/wlan0/address)"
+  log_progress "Configuring AP on $WLAN with IP $IP"
+
+  # Dynamically select the correct MAC address
+  if [ "$WLAN" = "wlan1" ]; then
+    log_progress "Using wlan1 for AP"
+    MAC="$(cat /sys/class/net/wlan1/address)"
+  else
+    log_progress "Using wlan0 for AP"
+    MAC="$(cat /sys/class/net/wlan0/address)"
+  fi
+
   cat <<- EOF > /etc/udev/rules.d/70-persistent-net.rules
 	SUBSYSTEM=="ieee80211", ACTION=="add|change", ATTR{macaddress}=="$MAC", KERNEL=="phy0", \
 	RUN+="/sbin/iw phy phy0 interface add ap0 type __ap", \
@@ -121,7 +127,7 @@ then
 
   cat <<- EOF > /etc/dnsmasq.conf
 	interface=lo,ap0
-	no-dhcp-interface=lo,wlan1,wlan0
+	no-dhcp-interface=lo,$WLAN
 	bind-interfaces
 	bogus-priv
 	dhcp-range=${NET}.10,${NET}.254,12h
@@ -135,14 +141,13 @@ then
 	driver=nl80211
 	ssid=${AP_SSID}
 	hw_mode=g
-	channel=11
-	wmm_enabled=0
-	macaddr_acl=0
+	channel=6
+	wmm_enabled=1
 	auth_algs=1
 	wpa=2
 	wpa_passphrase=${AP_PASS}
 	wpa_key_mgmt=WPA-PSK
-	wpa_pairwise=TKIP CCMP
+	wpa_pairwise=CCMP
 	rsn_pairwise=CCMP
 	EOF
   cat <<- EOF > /etc/default/hostapd
@@ -154,8 +159,7 @@ then
 
 	auto lo
 	auto ap0
-	auto wlan1
-	auto wlan0
+	auto $WLAN
 	iface lo inet loopback
 
 	allow-hotplug ap0
@@ -164,21 +168,34 @@ then
 	    netmask 255.255.255.0
 	    hostapd /etc/hostapd/hostapd.conf
 
-	allow-hotplug wlan1
-	iface wlan1 inet manual
+	allow-hotplug $WLAN
+	iface $WLAN inet manual
 	    wpa-roam /etc/wpa_supplicant/wpa_supplicant.conf
 	iface AP1 inet dhcp
-
-	allow-hotplug wlan0
-	iface wlan0 inet manual
-	    wpa-roam /etc/wpa_supplicant/wpa_supplicant.conf
-	iface AP2 inet dhcp
 	EOF
 
   cat <<- EOF >> /etc/dhcpcd.conf
 	interface ap0
 	nohook wpa_supplicant
+
+	interface $WLAN
+	nohook wpa_supplicant
 	EOF
+
+  if [ ! -L /var/lib/misc ]
+  then
+    if ! findmnt --mountpoint /mutable
+    then
+        mount /mutable
+    fi
+    mkdir -p /mutable/varlib
+    mv /var/lib/misc /mutable/varlib
+    ln -s /mutable/varlib/misc /var/lib/misc
+  fi
+
+  sed -i -e "/^127.0.0.1\s*localhost/b; s/^127.0.0.1\(\s*.*\)/$IP\1/" /etc/hosts
+
+  sed -i -e 's/}/  id_str="AP1"\n}/'  /etc/wpa_supplicant/wpa_supplicant.conf
 else
   log_progress "AP mode already configured"
 fi
