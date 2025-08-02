e4123d38016e2e126d79f9e18e8fef0b0611ec4a
THX723 <25112621+THX723@users.noreply.github.com>
2025-08-01 17:07:58 -0700

    Update disconnect-archive.sh
    Fixed flawed logic stuck on initial test, if mount point is already inaccessible.
    Added timeout for umount and streamlined the code.

--- run/cifs_archive/disconnect-archive.sh
+++ run/cifs_archive/disconnect-archive.sh
@@ -1,29 +1,26 @@
 #!/bin/bash -eu
 
-# unmount the archive. Without this, the archive mounts can get into a
+# Unmount the archive. Without this, the archive mounts can get into a
 # state where the archive is reachable via the network, appears to be
 # mounted, but the mount is inoperable and any attempt to access it
 # results in a "host is down" message.
 # Run this in the background, since unmounting can hang, which would
 # block a return to archiveloop.
 
+unmount_if_set() {
+  local mount_point=$1
+  if [ -n "$mount_point" ]
+  then
+    if timeout 30 umount -f -l "$mount_point"
+    then
+      log "$mount_point unmounted."
+    else
+      log "$mount_point failed to unmount!"
+    fi
+  fi
+}
+
 {
-  if [ -e "$ARCHIVE_MOUNT" ]
-  then
-    log "unmounting $ARCHIVE_MOUNT"
-    if ! umount -f -l "$ARCHIVE_MOUNT"
-    then
-      log "unmount failed"
-    fi
-  fi
-
-  if [ -e "$MUSIC_ARCHIVE_MOUNT" ]
-  then
-    log "unmounting $MUSIC_ARCHIVE_MOUNT"
-    if ! umount -f -l "$MUSIC_ARCHIVE_MOUNT"
-    then
-      log "unmount failed"
-    fi
-  fi
+  unmount_if_set "${ARCHIVE_MOUNT:-}"
+  unmount_if_set "${MUSIC_ARCHIVE_MOUNT:-}"
 } &
-
