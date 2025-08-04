835fe19ffdd1ca27ec4346ef3fc303d4a31137bc
THX723 <25112621+THX723@users.noreply.github.com>
2025-08-03 19:45:14 -0700

    Update connect-archive.sh
    Added exit codes.

--- run/cifs_archive/connect-archive.sh
+++ run/cifs_archive/connect-archive.sh
@@ -7,12 +7,15 @@ function mount_if_set() {
     if findmnt --mountpoint "$mount_point" > /dev/null
     then
       log "$mount_point is already mounted."
+      exit 0
     else
       if timeout 10 mount "$mount_point" >> "$LOG_FILE" 2>&1
       then
         log "Mounted $mount_point."
+        exit 0
       else
-        log "Failed to mount $mount_point."
+        log "Failed to umount $mount_point."
+        exit 1
       fi
     fi
   fi
