21ae8c7812a0fdafc623b18d5aca58762d58b729
THX723 <25112621+THX723@users.noreply.github.com>
2025-08-03 21:23:55 -0700

    Update connect-archive.sh
    Reduced code by using function already exists in archiveloop.

--- run/cifs_archive/connect-archive.sh
+++ run/cifs_archive/connect-archive.sh
@@ -4,19 +4,9 @@ function mount_if_set() {
   local mount_point=$1
   if [ -n "$mount_point" ]
   then
-    if findmnt --mountpoint "$mount_point" > /dev/null
+    if ! ensure_mountpoint_is_mounted "$mount_point"
     then
-      log "$mount_point is already mounted."
-      exit 0
-    else
-      if timeout 10 mount "$mount_point" >> "$LOG_FILE" 2>&1
-      then
-        log "Mounted $mount_point."
-        exit 0
-      else
-        log "Failed to umount $mount_point."
-        exit 1
-      fi
+      exit 1
     fi
   fi
 }
