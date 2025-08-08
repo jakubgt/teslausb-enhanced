38d0359742dc7f837c82a6643f2f5b3293dcd0d1
THX723 <25112621+THX723@users.noreply.github.com>
2025-08-07 18:54:00 -0700

    Update connect-archive.sh

--- run/cifs_archive/connect-archive.sh
+++ run/cifs_archive/connect-archive.sh
@@ -2,7 +2,7 @@
 
 function mount_if_set() {
   local mount_point=$1
-  [ -n "$mount_point" ] && ensure_mountpoint_is_mounted_with_retry "$mount_point"
+  [ -z "$mount_point" ] || ensure_mountpoint_is_mounted_with_retry "$mount_point"
 }
 
 mount_if_set "${ARCHIVE_MOUNT:-}"
