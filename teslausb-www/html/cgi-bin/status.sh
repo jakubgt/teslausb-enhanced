5edf6ad64eb5475937cd3825bf6df4b28e0496f3
DennisGarvey <32199720+DennisGarvey@users.noreply.github.com>
2026-04-26 19:53:34 -0400

    add sudo for vcgencmd

--- teslausb-www/html/cgi-bin/status.sh
+++ teslausb-www/html/cgi-bin/status.sh
@@ -59,7 +59,7 @@ Content-type: application/json
    "cpu_temp": "$(cat /sys/class/thermal/thermal_zone0/temp)",
    "fan_speed": "$fan_speed",
    "external_5v": "$external_5v",
-   "throttled": "$(vcgencmd get_throttled 2>/dev/null | sed -n 's/^throttled=//p' || echo "N/A")",
+   "throttled": "$(sudo -n vcgencmd get_throttled 2>/dev/null | sed -n 's/^throttled=//p' || echo "N/A")",
    "rtc_batt_v": "$rtc_batt_v",
    "num_snapshots": "$numsnapshots",
    "snapshot_oldest": "$oldestsnapshot",
